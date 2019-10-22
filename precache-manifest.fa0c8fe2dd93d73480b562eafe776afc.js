689400ab9e234058c812c980153a4b2bbe2483e5
Gajendra Ojha <gogypsum@gmail.com>
2019-10-23 01:08:25 +0530

    Updates

---
+++ precache-manifest.fa0c8fe2dd93d73480b562eafe776afc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ebc13ef29bc9d3994fcf920954a9aa",
+    "url": "/robojunkyard/index.html"
+  },
+  {
+    "revision": "b2e67ec753c6903dd38b",
+    "url": "/robojunkyard/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "9b6adb28d647fc453f84",
+    "url": "/robojunkyard/static/css/main.ac870e13.chunk.css"
+  },
+  {
+    "revision": "b2e67ec753c6903dd38b",
+    "url": "/robojunkyard/static/js/2.0cc91280.chunk.js"
+  },
+  {
+    "revision": "9b6adb28d647fc453f84",
+    "url": "/robojunkyard/static/js/main.e4935095.chunk.js"
+  },
+  {
+    "revision": "4c971da4da04b26734f9",
+    "url": "/robojunkyard/static/js/runtime~main.7e184920.js"
+  },
+  {
+    "revision": "71cc69d79e4d715ad33da22c9986f381",
+    "url": "/robojunkyard/static/media/DigitaldreamFat.71cc69d7.woff"
+  }
+]);
